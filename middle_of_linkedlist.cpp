ee2a49946e1dcb047cad8ff56edc3f7f4d1b21f3
Nitin Garg <nitin1960.be20@chitkara.edu.in>
2023-06-12 22:34:38 +0530

    added middle_of_linkedlist code in cpp

---
+++ middle_of_linkedlist.cpp
@@ -0,0 +1,34 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node *findMiddle(Node *head) {
+    Node* slow=head,*fast=head;
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
